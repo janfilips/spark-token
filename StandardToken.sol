05ad6a4a498007c08aaec1c0e97a08fbc2790881
Jan Paricka <jparicka@gmail.com>
2018-05-23 00:32:14 +0200

    added on erc223 stuff ontot the standard token

--- StandardToken.sol
+++ StandardToken.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.4.10;
 
 import 'browser/SafeMath.sol';
 import 'browser/ERC20.sol';
+import 'browser/ERC223.sol';
+import 'browser/ERC223ReceivingContract.sol';
 
-contract StandardToken is ERC20 {
+contract StandardToken is ERC20, ERC223 {
   using SafeMath for uint;
      
     string internal _name;
@@ -14,7 +16,7 @@ contract StandardToken is ERC20 {
     mapping (address => uint256) internal balances;
     mapping (address => mapping (address => uint256)) internal allowed;
 
-    function StandardToken() public {
+    function StandardToken(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
         _symbol = "SNY";
         _name = "SparkCoin";
         _decimals = 2;
@@ -101,5 +103,26 @@ contract StandardToken is ERC20 {
      Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
      return true;
    }
+   
+  function transfer(address _to, uint _value, bytes _data) public {
+    require(_value > 0 );
+    if(isContract(_to)) {
+        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+        receiver.tokenFallback(msg.sender, _value, _data);
+    }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value, _data);
+    }
+    
+  function isContract(address _addr) private returns (bool is_contract) {
+      uint length;
+      assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+      }
+      return (length>0);
+    }
+
 
 }
